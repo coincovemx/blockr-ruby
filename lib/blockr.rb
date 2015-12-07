b98a126de70894f7d118637fd7c60c104d2bfd51
Genaro Madrid <genaro@appio.mx>
2015-12-07 14:33:49 -0600

    add autoload and default methods

--- lib/blockr.rb
+++ lib/blockr.rb
@@ -1,5 +1,21 @@
-require 'blockr/version'
+require 'rest-client'
+require 'json'
 
 module Blockr
+  autoload :API,        'blockr/api'
+  autoload :Connection, 'blockr/connection'
+
+  attr_writer :api
+
+  API_VERSION = 'v1'
+  BLOCK_CHAIN = 'btc'
+
+  def self.api
+    @api ||= API.new(network:BLOCK_CHAIN, api_version:API_VERSION)
+  end
+
+  def self.method_missing(sym, *args, &block)
+    api.send(sym, *args, &block)
+  end
 
 end
